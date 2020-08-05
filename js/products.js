8bc16e0d8572c46318df49c5710217dda56b88b2
Pablo Díaz <Pablo.dc1991@gmail.com>
2020-08-05 16:10:55 -0300

    Update products.html y js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,28 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+let productArray = [];
 
+function showProducts(arry) {
+    let contenido = "";
+    for (let i = 0 ; i < arry.length ; i++){
+        let productList = array[i];
+
+        contenido += `
+        <div class="product-container">
+            <h3>`+ productList.name +`</h3>
+            <p>`+ productList.description +`</p>
+            <p>` + productList.cost +`</p>
+        `
+        document.getElementById("product-list").innerHTML = contenido;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productArray = resultObj.data;
+            showProducts(productArray);
+        }
+    })
 });
