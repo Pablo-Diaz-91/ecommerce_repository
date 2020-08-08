f5b481ce262c90929e41913eab5743a8c27b25c5
Pablo Díaz <Pablo.dc1991@gmail.com>
2020-08-08 02:05:39 -0300

    Update Product.js
    Se agregó la funcionalidad de Slides y se retocó la legibilidad del documento

--- js/products.js
+++ js/products.js
@@ -1,23 +1,57 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
 let productArray = [];
+
+/*SLIDES*/
+let c = 0;
+let time = 2500;
+
+function changeImg(){
+    let reference = document.getElementById("reference-img");
+    let slideTitle = document.getElementById("slide-title");
+
+    slideTitle.innerHTML = productArray[c].name;
+    document.slide.src = productArray[c].imgSrc;
+    reference.href = "#" + c;
+    
+    c++;
+
+    if (c > productArray.length - 1) {
+        c=0;
+    }
+
+    setTimeout(changeImg, time);
+
+}
+
+
+/*PRODUCTOS*/
 
 function showProducts(array) {
     let contenido = "";
+
     for (let i = 0 ; i < array.length ; i++){
         let productList = array[i];
-
+        
         contenido += `
-        <div class="product-container">
-            <img src=`+ productList.imgSrc +`>
-            <h3>`+ productList.name +`</h3>
-            <p>Descripción: `+ productList.description +`</p>
-            <p>Precio: `+ productList.currency +` `+ productList.cost +`</p>
-        `
+            <div class="product-content">
+                <h3 id=`+ i +`>`+ productList.name +`</h3>
+                    <div class="product-img-description">
+                        <img class="product-img" src=`+ productList.imgSrc +`>
+                        <p class="description">Descripción: `+ productList.description +`</p>
+                    </div>
+                <p class="price"><strong>Precio: </strong>`+ productList.currency +` `+ productList.cost +`</p>
+            </div>
+            `
         document.getElementById("product-list").innerHTML = contenido;
+        
     }
+    changeImg();
 }
+
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function (resultObj) {
