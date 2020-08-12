6c9575cb6f29c0601ece08b11201c981912e5bb3
Pablo Díaz <Pablo.dc1991@gmail.com>
2020-08-12 14:51:48 -0300

    login como portada
    Se renombraron archivos. Login como index.html e index como cover.html. Se arreglaron los enlaces de navegación en todos los html para que el inicio (en la barra) sea el cover.html

--- js/login.js
+++ js/login.js
@@ -2,5 +2,19 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("btn-sign-in").addEventListener("click", function (e) {
 
+        let inputEmail = document.getElementById("email");
+        let inputPassword = document.getElementById("password");
+        let camposCompletos = true;
+
+        if (inputEmail.value === '' || inputPassword.value === '') {
+            camposCompletos = false;
+            alert('Por favor complete los campos');
+        }
+
+        if (camposCompletos) {
+            window.location = 'cover.html';
+        }
+    });
 });
