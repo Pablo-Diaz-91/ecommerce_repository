2755f2d93732ad703da1b1ea856b00a40a86e484
Pablo Díaz <Pablo.dc1991@gmail.com>
2020-09-08 20:27:13 -0300

    Parcial showProductDetails
    Se agregó la función showProductDetails de forma parcial (falta dibujar los comentarios) y los getJSONData que reciben la información desde las URL correspondientes.

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,59 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+productArray = [];
+commentArray = [];
 
+function showProductDetails(products, comments) {
+    let commentsDisplay = '';
+    let productDetails = '';
+    
+    productDetails += `
+                        <h3>${products.name}</h3>
+                        <div class="description">
+                            <p>Descripción: ${products.description}</p>
+                            <p>Precio: ${products.currency} ${products.cost}
+                        </div>
+                        <div class="images">
+                            <img src="img/prod1.jpg" alt="Imagen del producto">
+                            <img src="img/prod1_1.jpg"
+                            alt="Imagen del producto" >
+                            <img src="img/prod1_2.jpg"
+                            alt="Imagen del producto" >
+                            <img src="img/prod1_3.jpg"
+                            alt="Imagen del producto" >
+                            <img src="img/prod1_4.jpg"
+                            alt="Imagen del producto" >
+                        </div>
+                    `;
+    
+    /*for (let comment in comments) {
+        commentsDisplay = `
+                            <h5>${comments[comment].user}</h5>
+                        `;
+                    }
+   */
+
+
+    document.getElementById("details-container").innerHTML = productDetails;
+    //document.getElementById("comments").innerHTML = commentsDisplay;
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productArray = resultObj.data;
+        }
+    });
 });
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            commentArray = resultObj.data;
+
+            showProductDetails(productArray, commentArray);
+        }
+    })
+})
