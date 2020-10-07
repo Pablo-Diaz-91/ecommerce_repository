7c424949f22f0d151f55fcd529523e871f3cf64d
Pablo Díaz <Pablo.dc1991@gmail.com>
2020-10-07 16:24:01 -0300

    Fixed table
    Movimos el cuerpo de la tabla al html para que no nos dibuje varias tablas por elemento.

--- js/cart.js
+++ js/cart.js
@@ -15,7 +15,7 @@ function calcTotal(){
 function calcSubtotal(price, i) {
     let amount = parseInt(document.getElementById(`amount${i}`).value);
     subtotal = amount * price;
-    document.getElementById(`productsSubtotal${i}`).innerHTML = subtotal;
+    document.getElementById(`productsSubtotal${i}`).innerHTML = subtotal + " UYU";
     calcTotal();
 }
 
@@ -27,18 +27,7 @@ function showCartContent(array) {
         let sub = productsCart.unitCost * productsCart.count;
 
         content += `
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col"> # </th>
-                        <th scope="col"> Producto </th> 
-                        <th scope="col"> Precio por unidad </th> 
-                        <th scope="col"> Cantidad </th>
-                        <th scope="col"> Subtotal </th> 
-                    </tr>
-                </thead>
-
-                <tbody>
+            
                     <tr>
                         <th scope="row"><img src="${productsCart.src}" alt="Imagen del producto" width="30px"></th>
                         
@@ -50,16 +39,9 @@ function showCartContent(array) {
                         
                         <td> <span class="subtotal" id="productsSubtotal${i}">${sub + " " + productsCart.currency}</span> </td>
                         
-                        <td> <button>&times</button> </td>
+                        <td> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> </td>
                     </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <th colspan="4">Total</th>
-                        <td id="total"> </td>
-                    </tr>
-                </tfoot>
-            </table>
+                
         `
 
         document.getElementById("cart-table").innerHTML = content;
