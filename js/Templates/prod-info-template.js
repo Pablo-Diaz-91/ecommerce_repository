e2839a793f7d6646b38e371806217b8c13803692
Pablo Díaz <Pablo.dc1991@gmail.com>
2020-10-30 19:53:56 -0300

    First stage responsive
    Se agregó funciones de responsive design, primera etapa

--- js/Templates/prod-info-template.js
+++ js/Templates/prod-info-template.js
@@ -1,7 +1,7 @@
 function productDetailsTemp(products){
     return `
             <h3>${products.name}</h3>
-            <div class="description">
+            <div>
                 <p>Descripción: ${products.description}</p>
                 <p class="detailsPrice"><strong>Precio: ${products.currency} ${products.cost}</strong></p>
             </div>
@@ -9,18 +9,20 @@ function productDetailsTemp(products){
 }
 
 function relatedProducts(products, i){
-    return `
-                    <div class="relatedProd">
+    return `    <div class="col-lg-6 col-12">
                         <a href="#">
-                            <img class="imgRelated" src="${products[i].imgSrc}">
-                            <div class="relatedDescript">
-                                <h4>${products[i].name}</h4>
-                                <p>${products[i].description}</p>
-                                <p>Precio: ${products[i].currency} ${products[i].cost}</p>
-                                <p>Cantidad vendidos: ${products[i].soldCount}</p>
+                            <div class="row border">
+                                <div class="col-lg-6 col-12">
+                                    <img class="imgRelated" src="${products[i].imgSrc}">
+                                </div>
+                                <div class="col-lg-6 col-12 relatedDescript">
+                                    <h4>${products[i].name}</h4>
+                                    <p>${products[i].description}</p>
+                                    <p>Precio: ${products[i].currency} ${products[i].cost}</p>
+                                    <p>Cantidad vendidos: ${products[i].soldCount}</p>
+                                </div>
                             </div>
                         </a>
-                    </div>
-                    
+                </div>
                 `;
 }
--- js/Templates/product-templates.js
+++ js/Templates/product-templates.js
@@ -1,15 +1,31 @@
 function productShown(productList, i){
     return `
-            <div class="product-content">
-                <h3 id=${i}>${productList.name}</h3>
-                    <div class="product-img-description">
-                        <img class="product-img" src=${productList.imgSrc} alt="Productos">
-                        <p class="description"> Descripción: ${productList.description}</p>
-                        
+        <div class="col-md-5">
+
+            <div class="row my-3">
+                <div class="product-content my-auto">
+                    <div class="col-12 mb-3">
+                        <img style="width:100%" class="product-img" src=${productList.imgSrc} alt="Productos">
                     </div>
-                <p class="price"><strong> Precio: </strong>${productList.currency}  ${productList.cost}</p>
-                <p class="soldCount">Cantidad vendidos: ${productList.soldCount}</p>
-                <button class="productDetails" onclick="productDetails()">Ver producto</button>
+                    <div class="col-12">
+                        <h3 id=${i}>${productList.name}</h3>
+                    </div>
+                    <div class="col-12">
+                        <p class="description"> Descripción: ${productList.description}</p>
+                    </div>
+                    <div class="col-12">
+                        <p class="price"><strong> Precio: </strong>${productList.currency}  ${productList.cost}</p>
+                    </div>
+                    <div class="col-12">
+                        <p class="soldCount">Cantidad vendidos: ${productList.soldCount}</p>
+                    </div>
+                    <div class="col-12">
+                        <button class="btn-block productDetails" onclick="productDetails()">Ver producto</button>
+                    </div>
+                </div>
             </div>
+
+        </div>
+
             `;
 }
